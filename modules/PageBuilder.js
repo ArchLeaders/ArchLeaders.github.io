3b2162dc617e081b9840b2614d0ab71f3853e559
ArchLeaders <archparliament@gmail.com>
2023-02-17 20:29:39 -0800

    Refactor page builder (pending rename)

--- modules/PageBuilder.js
+++ modules/PageBuilder.js
@@ -1,24 +1,43 @@
 import { Octokit, App } from "https://cdn.skypack.dev/octokit";
 
+let octokit = new Octokit();
+let repos = [];
 
 export class PageBuilder {
-    static async BuildRepoList() {
-        let octokit = new Octokit();
-        let request = await octokit.request("GET /users/{username}/repos", {
-            username: "ArchLeaders",
-            type: "forks",
-            per_page: 40,
-            sort: "pushed"
-        });
 
-        let repos = request.data;
-        let ul = document.getElementById("github-repos");
+    static async LoadRepos(username, type) {
+        let user = await octokit.request("GET /{_type}/{username}", {
+            _type: type,
+            username: username
+        }).then(x => x.data);
+
+        let page = 1;
+        while (repos.length < user.public_repos) {
+            let _repos = await octokit.request("GET /{_type}/{username}/repos", {
+                _type: type,
+                username: username,
+                type: 'public',
+                per_page: user.public_repos > (100 * page) ? 100 : user.public_repos - (100 * (page - 1)),
+                page: page
+            }).then(x => x.data);
+
+            repos = repos.concat(_repos);
+            page++;
+        }
+
+        console.log(`Received ${repos.length} repositories from 'GET /${type}/${username}/repos'`)
+    }
+
+    static async BuildProjectsPage(topic) {
+        await this.LoadRepos("ArchLeaders", 'users');
+
+        let elements = '';
         repos.forEach(repo => {
-            if (repo.fork) {
+            if (repo.fork || !repo.topics.includes(topic)) {
                 return;
             }
 
-            ul.innerHTML += this.BuildCardHtml(
+            elements += this.BuildCardHtml(
                 repo.name,
                 repo.html_url,
                 repo.description ?? "None",
@@ -26,6 +45,8 @@ export class PageBuilder {
                 repo.license?.name ?? "None"
             )
         });
+
+        document.getElementById("github-repos").innerHTML = elements;
     }
 
     static BuildCardHtml(title, http, desc, lang, license) {
